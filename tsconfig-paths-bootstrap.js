4bbe20e096f835921a20e300cdd772614f0a3da6
Amr2812 <ana.osama.elmohamady@gmail.com>
2022-07-04 15:28:33 +0200

    uninstall module-alias tsconfig-paths

--- tsconfig-paths-bootstrap.js
+++
@@ -1,8 +0,0 @@
-const tsConfigPaths = require("tsconfig-paths");
-const tsConfig = require("./tsconfig.json");
-
-const baseUrl = "./dist";
-tsConfigPaths.register({
-  baseUrl,
-  paths: tsConfig.compilerOptions.paths
-});
