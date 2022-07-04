6eb36d996cbac74034cec21dba07bf6766eac290
Amr2812 <ana.osama.elmohamady@gmail.com>
2022-07-04 14:25:29 +0200

    fix: tsconfig-paths

---
+++ tsconfig-paths-bootstrap.js
@@ -0,0 +1,8 @@
+const tsConfigPaths = require("tsconfig-paths");
+const tsConfig = require("./tsconfig.json");
+
+const baseUrl = "./dist";
+tsConfigPaths.register({
+  baseUrl,
+  paths: tsConfig.compilerOptions.paths
+});
