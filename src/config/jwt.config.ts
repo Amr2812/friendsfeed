15623bdb63dc18d32ee6501402669f0420aabd68
Amr2812 <ana.osama.elmohamady@gmail.com>
2022-07-15 10:29:23 +0200

    Update JWT expiresIn in dev to have longer time

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -3,7 +3,10 @@ import { registerAs } from "@nestjs/config";
 export const jwtConfig = registerAs("jwt", () => ({
   accessToken: {
     secret: process.env.JWT_ACCESS_SECRET,
-    expiresIn: 1000 * 60 * 15 // 15 minutes
+    expiresIn:
+      process.env.NODE_ENV == "development"
+        ? 1000 * 60 * 60 * 2 // 2 hours
+        : 1000 * 60 * 15 // 15 minutes
   },
   refreshToken: {
     secret: process.env.JWT_REFRESH_SECRET,
