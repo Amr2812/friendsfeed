c6a5246317637f0e36cccf658d76d4022ab0c0cb
Amr Elmohamady <ana.osama.elmohamady@gmail.com>
2024-01-25 16:31:42 +0200

    fix: production db sync

--- core/ormconfig.ts
+++ core/ormconfig.ts
@@ -8,6 +8,7 @@ const config: PostgresConnectionOptions = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  synchronize: true,
   entities: ["dist/src/**/*.entity{.ts,.js}"],
   migrations: ["dist/src/database/migrations/*{.ts,.js}"],
   cli: {
